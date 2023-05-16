d0480d3c9ed45f90fe4e2d2f62098bcd57ef78f3
Ervin <ervin.ceriola@arcadian.la>
2023-05-16 16:08:37 +0800

    ready for library deployment

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'component-library.js',
+    library: 'component library',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+};
